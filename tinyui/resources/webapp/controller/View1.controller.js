fb406a9856c382c97ad19449216c6f89f0669b70
LukWet <mokirs3@gmail.com>
2021-03-28 16:35:55 +0200

    Commit 2.0

--- tinyui/resources/webapp/controller/View1.controller.js
+++ tinyui/resources/webapp/controller/View1.controller.js
@@ -2,12 +2,35 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function (Controller) {
 	"use strict";
-
 	return Controller.extend("tinyworld.tinyui.controller.View1", {
 		onInit: function () {
-			  var oModel = new sap.ui.model.odata.ODataModel("/euro.xsodata", true);
-			  var oView = this.getView();
-			  oView.setModel(oModel);
+			var oModel = new sap.ui.model.odata.ODataModel("/euro.xsodata", true);
+			var oView = this.getView();
+			oView.setModel(oModel);
+		},
+		onPress: function () {
+			var oInput = this.getView().byId("input").getValue();
+			var name = oInput;
+			var partof = "Europe";
+			var payload = {};
+			payload.name = name;
+			payload.partof = partof;
+			var insertdata = JSON.stringify(payload);
+			$.ajax({
+				type: "POST",
+				url: "/country/country.xsjs",
+				contentType: "application/json",
+				data: insertdata,
+				dataType: "json",
+				crossDomain: true,
+				success: function (data) {
+					alert("Data inserted successfully");
+				},
+				error: function (data) {
+					var message = JSON.stringify(data);
+					alert(message);
+				}
+			});
 		}
 	});
 });
